11245e33e09a80fd424ede72cf354b54c87e20b3
castle.jaemin <ehdrud1332@gmail.com>
2021-03-02 15:17:37 +0900

    #1 firebase 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdvZTzQmdyCoWKRzLOF_zKF0nGEJ8nbkU",
+    authDomain: "twitter-93fd8.firebaseapp.com",
+    projectId: "twitter-93fd8",
+    storageBucket: "twitter-93fd8.appspot.com",
+    messagingSenderId: "828697918759",
+    appId: "1:828697918759:web:eed28ecaed3aa410a9bd02"
+};
+
+export default firebase.initializeApp(firebaseConfig);
