62cd5edb9034ee330eb7678327ff4e8dc9a21516
castle.jaemin <ehdrud1332@gmail.com>
2021-03-02 15:54:34 +0900

    #2 Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdvZTzQmdyCoWKRzLOF_zKF0nGEJ8nbkU",
-    authDomain: "twitter-93fd8.firebaseapp.com",
-    projectId: "twitter-93fd8",
-    storageBucket: "twitter-93fd8.appspot.com",
-    messagingSenderId: "828697918759",
-    appId: "1:828697918759:web:eed28ecaed3aa410a9bd02"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
