a4671d7b131abc55ec8f688f54a2f1667b7fb56d
castle.jaemin <ehdrud1332@gmail.com>
2021-03-03 20:16:08 +0900

    #5-2 tweet component 생성
    owner를 부여해서 owner만 update, delete 가능하게 하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { dbService } from "../fbase";
+import Tweet from "../components/Tweet";
 
 const Home = ({ userObj }) => {
 
@@ -42,9 +43,11 @@ const Home = ({ userObj }) => {
             </form>
             <div>
                 {tweets.map(tweet =>
-                    <div key={tweet.id}>
-                        <h4>{tweet.text}</h4>
-                    </div>
+                    <Tweet
+                        key={tweet.id}
+                        tweetObj={tweet}
+                        isOwner={tweet.creatorId === userObj.uid}
+                    />
                 )}
             </div>
         </div>
