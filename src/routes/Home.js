03e352c28f30258208a1ad8803fe2dc1555df20c
castle.jaemin <ehdrud1332@gmail.com>
2021-03-03 16:31:12 +0900

    #5 Get tweets
    함수 생성해서 트윗 불러오고 나타내기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { dbService } from "../fbase";
 
 export default () => {
 
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+
+    const getTweets = async() => {
+        // state에 있는 각각의 documents.data()를 console.log 하고 있다.
+        const dbtTweets = await dbService.collection("tweets").get();
+        // 모든 이전 tweets에 대해 배열을 return 할건데 그 배열안에는 새로작성한 tweets & ...prev(그 이전의 것)
+        // set~~~ 을 쓸때 값 대신 함수를 전달 할 수 있다. 함수를 전달하면 react는 이전 값에 접근할 수 있게 해줌.
+        dbtTweets.forEach(document => {
+            const tweetsObject = {
+                ...document.data(),
+                id : document.id
+            }
+            setTweets((prev) => [tweetsObject, ...prev])
+        })
+    }
+
+    useEffect(() => {
+        getTweets()
+    }, [])
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -11,12 +30,13 @@ export default () => {
             tweet,
             createAt: Date.now()
         });
-        setTweet("");  
+        setTweet("");
     };
     const onChange = (event) => {
         const { target : {value}} = event;
         setTweet(value);
     }
+    console.log(tweets)
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -29,6 +49,13 @@ export default () => {
                 />
                 <input type='submit' value="twitter"/>
             </form>
+            <div>
+                {tweets.map(tweet =>
+                    <div key={tweet.id}>
+                        <h4>{tweet.tweet}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
