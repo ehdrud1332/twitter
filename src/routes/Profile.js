eab253c4065405085ac8f5604111501101385966
castle.jaemin <ehdrud1332@gmail.com>
2021-03-04 16:38:52 +0900

    #7 Profile displayName 업데이트

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {authService, dbService} from "../fbase";
 import { useHistory } from 'react-router-dom';
 
 const Profile =  ({ userObj }) => {
+
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
 
     const history = useHistory();
     const onLogoutClick = () => {
@@ -23,8 +25,34 @@ const Profile =  ({ userObj }) => {
         getMyTweets();
     }, []);
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName
+            })
+        }
+    };
+
+    const onChange = (event) => {
+        const {target: {value}} = event;
+        setNewDisplayName(value)
+    }
+
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Display Name"
+                    value={newDisplayName}
+                />
+                <input
+                    type="submit"
+                    value="Update Profile"
+                />
+            </form>
             <button onClick={onLogoutClick}>Log Out</button>
         </>
     )
